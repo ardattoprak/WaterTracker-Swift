c8bc54b889bd9b62ea511a0a13e27ef1a279437c
ardatoprak <ardattoprak@gmail.com>
2023-01-18 21:32:42 +0300

    user interface design completed

---
+++ Water Tracker/DataStore.swift
@@ -0,0 +1,22 @@
+//
+//  DataStore.swift
+//  Water Tracker
+//
+//  Created by Arda Toprak on 18.01.2023.
+//
+
+import Foundation
+
+class DataStore {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    
+    // MARK: - Functions
+
+    
+}
--- Water Tracker/ViewController.swift
+++ Water Tracker/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - UI Elements
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var addWaterConstraint: NSLayoutConstraint!
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    // MARK: - Functions
+    
+    @IBAction func addWaterButtonTapped(_ sender: Any) {
+    }
+    
+    // MARK: - Actions
 
 }
 
