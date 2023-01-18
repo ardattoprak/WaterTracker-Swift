1e0665d592f2f69067ce876e26d55d3d43d46c76
ardatoprak <ardattoprak@gmail.com>
2023-01-18 22:32:56 +0300

    final commit

--- Water Tracker/DataStore.swift
+++ Water Tracker/DataStore.swift
@@ -11,12 +11,40 @@ class DataStore {
     
     // MARK: - Properties
     
+    let defaults = UserDefaults.standard
     
+    var currentDay: Int {
+        let now = Date()
+        let day = Calendar.current.component(.day, from: now)
+        
+        return day
+    }
     
     // MARK: - Life Cycle
-    
+    init() {
+        let latestUpdateDay = getLatestUpdateDay()
+        
+        if getLatestUpdateDay() != currentDay {
+            defaults.set(0, forKey: "currentAmount")
+            
+        }
+    }
     
     // MARK: - Functions
-
-    
+    func getCurrentAmount() -> Double{
+        let savedAmount = defaults.double(forKey: "currentAmount")
+        
+        return savedAmount
+    }
+    func getLatestUpdateDay() -> Int{
+        let latestDay = defaults.integer(forKey: "latestUpdateDay")
+        return latestDay
+    }
+    func addWater(amount: Double) {
+        let currentAmount = getCurrentAmount()
+        let newAmount = currentAmount + amount
+        
+        defaults.set(newAmount, forKey: "currentAmount")
+        defaults.set(currentDay, forKey: "latestUpdateDay")
+    }
 }
